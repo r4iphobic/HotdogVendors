aab7f22f9c9a61996c648d20e4c8d510009333e0
r41 <caobroomes@outlook.com>
2026-04-20 15:55:26 +0100

    Function complete! Works also :)
    Character limit for hotdog vendor names is 15 characters.

--- Display Vendors Function.py
+++ Display Vendors Function.py
@@ -1,19 +1,22 @@
 #Display Vendors
 
-#Prepping the code to be read & manipulated:
+def displayVendors(file):
+    #Prepping the code to be read "&" manipulated:
 
-with open ("Hotdogs.txt", "r+") as data:      #opens file to read and write (also automatically closes)
-    currenthv =[]
-    currenthv = data.read().split("\n")       #splits the text file into a list by each new line
+    with open (file, "r+") as data:      #opens file to read and write (also automatically closes)
+        currenthv =[]
+        currenthv = data.read().split("\n")       #splits the text file into a list by each new line
 
-for i in range(0,(len(currenthv) -1) ):       #splits the text file into smaller lists by each ','
-    currenthv[i] = currenthv[i].split(",")
-
-title = "{:<20} {:^10} {:>20}       {:>20} {:>20} {:^15} {:^15}"
-values = "{:<20} {:^10} {:>20}       {:>20} {:>20} {:^15} {:^15}"
+    for i in range(0,(len(currenthv) -1) ):       #splits the text file into smaller lists by each ','
+        currenthv[i] = currenthv[i].split(",")
 
 
-print(title.format("Vendor ID", "Vendor Name", "Year and Week", "# of Vegan Hotdogs", "# of Meat Hotdogs", "Onions (kg)", "Ketchup (litres)"), f"\n{"-"*134}\n")
+    title = "{:<20} {:^15} {:>20}       {:>20} {:>20} {:^15} {:^15}"        #formats the titles (clarifying what each value in the file is)
+    values = "{:<20} {:^15}{:>20}       {:^20} {:^20} {:^15} {:^15}"        #formats the values within the file
 
-for i in range(0,(len(currenthv) -1)):
-    print(values.format(currenthv[i][0],currenthv[i][1],currenthv[i][2],currenthv[i][3],currenthv[i][4],currenthv[i][5],currenthv[i][6]))
+    print(title.format("Vendor ID", "Vendor Name", "Year and Week", "# of Vegan Hotdogs", "# of Meat Hotdogs", "Onions (kg)", "Ketchup (litres)"), f"\n{"-"*139}")
+
+    for i in range(0,(len(currenthv) -1)):
+        print(values.format(currenthv[i][0],currenthv[i][1],currenthv[i][2],currenthv[i][3],currenthv[i][4],currenthv[i][5],currenthv[i][6]))
+
+displayVendors("Hotdogs.txt")
