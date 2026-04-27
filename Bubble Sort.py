fbf0b8d38501dacdfb9a8d20d947d7e762a742e0
r41 <caobroomes@outlook.com>
2026-04-27 06:05:21 +0100

    Completed! Works :)

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,16 +1,27 @@
+#UNLINTED, UNVALIDATED Bubble Sort
+
+#---Prepping the code to be read & manipulated:
+
+with open ("Testhotdogs.txt", "r+") as data:      #opens file to read and write (also automatically closes)
+    currenthv =[]
+    currenthv = data.read().split("\n")       #splits the text file into a list by each new line
+
+for i in range(0,(len(currenthv) -1) ):       #splits the text file into smaller lists by each ','
+    currenthv[i] = currenthv[i].split(",")
+
 #---Bubble sort
 
-testlist = [75, 6, 20, 19]
-
 swap = True
-while (swap == True):
-    swap = False
-    for i in range (0, (len(testlist) - 1)):
-        if (testlist[i] > testlist[i+1]):
-            a = testlist[i]
-            testlist.remove(testlist[i])
-            testlist.insert(i+1, a)
-            swap = True
-        print(testlist)
+index = int(input("Input index: "))           #is the index of the value the array will be sorted by
 
-print("Final list is", testlist)
+while (swap == True):
+    swap = False                              #to stop the loop
+    for i in range(0, (len(currenthv) - 1)):
+        if ((currenthv[i][index]) > (currenthv[i+1][index])):
+            #compares the values in each sublist, swaps if left value is bigger than right value
+            a = currenthv[i]
+            currenthv.remove(currenthv[i])
+            currenthv.insert(i+1,a)
+            swap = True                       #to start it again if a swap has been made
+
+print(currenthv)
