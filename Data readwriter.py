9118dfca02a7b1849c19d7b83b1d9b3800f68268
r41 <caobroomes@outlook.com>
2026-04-29 17:04:02 +0100

    Update to only contain solution code

--- Data readwriter.py
+++ Data readwriter.py
@@ -1,24 +1,10 @@
 #Data reader/writer
 
-data = open("Hotdogs.txt", "r")         #opens file to read
-print(data.read())                      #prints out file data
-data.close()                            #closes file, important so that files don't risk corruption
-
-print("-------------------")
-#---------------------------
-
-data = open("Hotdogs.txt", "a+")        #"a+" opens file to read and write to the end of the file
-data.write("blahblahblah")
-data.seek(0)                            #moves file pointer to the beginning of the file
-print(data.read())
-print("-----------please-----------")
-#---------------------------
-
 with open ("Hotdogs.txt", "r+") as data:
-    currenthv =[]
-    currenthv = data.read().split("\n")
-    print(currenthv)
+    hotdogs =[]
+    hotdogs = data.read().split("\n")
+    print(hotdogs)
 
-for i in currenthv:
+for i in hotdogs:
     vendor = i.split(",")
     print(vendor)
