fe0e842f17dba7de6f1527a71b7ee8e9c55ab8b9
r41 <caobroomes@outlook.com>
2026-04-30 15:10:07 +0100

    Add files via upload

--- final_hotdog_vendors.py
+++ final_hotdog_vendors.py
@@ -60,4 +60,74 @@ def password_validation():
 
     return username
 
-password_validation()
+#Employee Menu function
+def employeeMenu():
+    """Employee Menu
+    Allows users to...
+    * View vendors
+    * Add new vendors
+    * Search for vendors"""
+
+    while True:
+        try:
+            choice = int(input("What would you like to do?: \n" \
+            "1. View Vendors\n" \
+            "2. Add Vendor\n" \
+            "3. Search for a Vendor\n"
+            "Input: "))
+        except ValueError:
+            print("Please enter an integer between 1 & 3 (inclusive).\n")
+
+
+        if (choice == 1):
+            displayVendors(currenthv)
+            break
+        elif (choice == 2):
+            addVendor()
+            break
+        elif (choice == 3):
+            searchVendor()
+            break
+        else:
+            print("Please enter an integer between 1 & 3 (inclusive).\n")
+    
+    while True:
+        repeat = input("Would you like to do anything else? Yes/No ")
+    
+        if (repeat == "Yes"):
+            employeeMenu()
+            break
+        elif(repeat == "No"):
+            break
+        else:
+            print("Please input 'Yes' or 'No'.\n")
+
+#UNLINTED Display Vendors function
+
+def displayVendors(array):
+    """displays multiple vendors (and asks to sort them)"""
+
+    #formats the titles (clarifying what each value in the file is)
+    title = "{:<20} {:^25} {:>20}       {:>20} {:>20} {:^15} {:^15}"
+
+    #formats the values within the file
+    values = "{:<20} {:^25}{:>20}       {:^20} {:^20} {:^15} {:^15}"
+
+    print(title.format("Vendor ID", "Vendor Name", "Year and Week", "# of Vegan Hotdogs", "# of Meat Hotdogs", "Onions (kg)", "Ketchup (litres)"), f"\n{"-"*139}")
+
+    for i in array:
+        if (type(i) == str):
+            i = i.split(",")
+
+        print(values.format(i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+    
+    while True:
+        sort = input("Sort vendors? Yes/No ")
+
+        if (sort == "Yes"):
+            displayVendors(sortVendors(currenthv))
+            break
+        elif (sort == "No"):
+            break
+        else:
+            print("Please input 'Yes' or 'No' (character sensitive)\n")
